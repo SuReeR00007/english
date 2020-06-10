2c27e118ff877969beb18cb2b384938391b53183
SuReeR00007 <surer00007@gmail.com>
2020-06-11 00:44:40 +0500

    Update words.blade.php
    таблица поправки

--- resources/views/words.blade.php
+++ resources/views/words.blade.php
@@ -25,99 +25,78 @@
                             <tr>
                                 <td class="word" data-word="bay">Bay</td>
                                 <td>Залив</td>
+                                <td class="word" data-word="pen">Pen</td>
+                                <td>Ручка</td>
+                                </tr>
                             </tr>
                             <tr>
                                 <td class="word" data-word="city">City</td>
                                 <td>Город</td>
+                                <td class="word" data-word="queen">Queen</td>
+                                <td>Королева</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="day">Day</td>
                                 <td>День</td>
+                                <td class="word" data-word="rain">Rain</td>
+                                <td>Дождь</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="eat">Eat</td>
                                 <td>Еда</td>
+                                <td class="word" data-word="snow">Snow</td>
+                                <td>Снег</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="fan">Fan</td>
                                 <td>Поклонник</td>
+                                <td class="word" data-word="text">Text</td>
+                                <td>Текст</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="gas">Gas</td>
                                 <td>Газ</td>
+                                <td class="word" data-word="use">Use</td>
+                                <td>Исполькзовать</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="hay">Hay</td>
                                 <td>Сено</td>
+                                <td class="word" data-word="very">Very</td>
+                                <td>Очень</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="ice">Ice</td>
                                 <td>Лед</td>
-                            </tr>
-                            <tr>
                                 <td class="word" data-word="jew">Jew</td>
                                 <td>Иудей</td>
                             </tr>
                             <tr>
                                 <td class="word" data-word="key">Key</td>
                                 <td>Ключ</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="lip">Lip</td>
-                                <td>Губа</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="my">My</td>
-                                <td>Мой</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="no">No</td>
-                                <td>Нет</td>
-                            </tr>
-                            
-                            <tr>
-                                <td class="word" data-word="pen">Pen</td>
-                                <td>Ручка</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="queen">Queen</td>
-                                <td>Королева</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="rain">Rain</td>
-                                <td>Дождь</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="snow">Snow</td>
-                                <td>Акт</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="text">Text</td>
-                                <td>Текст</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="use">Use</td>
-                                <td>Исполькзовать</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="very">Very</td>
-                                <td>Очень</td>
-                            </tr>
-                            <tr>
                                 <td class="word" data-word="wait">wait</td>
                                 <td>Подождите</td>
                             </tr>
                             <tr>
+                                <td class="word" data-word="lip">Lip</td>
+                                <td>Губа</td>
                                 <td class="word" data-word="x-ray">X-ray</td>
                                 <td>Рентген</td>
                             </tr>
                             <tr>
+                                <td class="word" data-word="my">My</td>
+                                <td>Мой</td>
                                 <td class="word" data-word="yes">Yes</td>
                                 <td>Да</td>
                             </tr>
                             <tr>
+                                <td class="word" data-word="no">No</td>
+                                <td>Нет</td>
                                 <td class="word" data-word="zoo">Zoo</td>
                                 <td>Зоопарк</td>
+                            </tr>
+
+
                         </tbody>
                     </table>
                 </div>
