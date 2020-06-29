2b6fc32c909ca4c0adbeef614dbe1a8a085e449b
SuReeR00007 <surer00007@gmail.com>
2020-06-29 11:46:40 +0500

    add description

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,16 +3,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
                 <li class="nav-item {{ $active['home'] }}">
-                    <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="./">Заглавная <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item {{ $active['abc'] }}">
-                    <a class="nav-link" href="./abc">ABC</a>
+                    <a class="nav-link" href="./abc">Алфавит</a>
                 </li>
                 <li class="nav-item {{ $active['words'] }}">
-                    <a class="nav-link" href="./words">Words</a>
+                    <a class="nav-link" href="./words">Слова</a>
                 </li>
                 <li class="nav-item {{ $active['translate'] }}">
-                    <a class="nav-link" href="./translate">Translate</a>
+                    <a class="nav-link" href="./translate">Тест</a>
                 </li>
             </ul>
         </div>
