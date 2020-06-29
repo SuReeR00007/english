bb05ebe13111d5db73fa1a02e3a5e2307960143f
SuReeR00007 <surer00007@gmail.com>
2020-06-29 22:50:36 +0500

    procfile

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,8 +1,6 @@
 <meta charset="utf-8">
-<meta name="description" content="">
-<meta name="author" content="Scotch">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>Super Cool Layouts</title>
+<title>App for learn English</title>
 <link rel="stylesheet" href="./css/style.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
