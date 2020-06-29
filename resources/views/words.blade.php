52d2d6628310f701393554171cfa6f87fdd9625a
SuReeR00007 <surer00007@gmail.com>
2020-06-29 11:59:00 +0500

    add description

--- resources/views/words.blade.php
+++ resources/views/words.blade.php
@@ -5,119 +5,124 @@
     </head>
     <body>
         @include('header')
-            <div id="main">
+        <div id="main">
+            <div class="d-flex">
                 <div class="p-2">
                     <img src="./public/img/sound.png" width="100" height="100">
                 </div>
-                <div>
+                <div class="p-2">
                     <h2>В английском языке написание слов за последние 400-500 
                         лет почти не изменилось, а звуковая сторона языка 
                         претерпела значительные изменения</h2>
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <td>Word</td>
-                                <td>Translate</td>
-                                <td></td>
-                                <td>Word</td>
-                                <td>Translate</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="act" title="Возпроизвести">Act</td>
-                                <td>Акт</td>
-                                <td></td>
-                                <td class="word" data-word="old" title="Возпроизвести">Old</td>
-                                <td>Старый</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="bay" title="Возпроизвести">Bay</td>
-                                <td>Залив</td>
-                                <td></td>
-                                <td class="word" data-word="pen" title="Возпроизвести">Pen</td>
-                                <td>Ручка</td>
-                                </tr>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="city" title="Возпроизвести">City</td>
-                                <td>Город</td>
-                                <td></td>
-                                <td class="word" data-word="queen" title="Возпроизвести">Queen</td>
-                                <td>Королева</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="day" title="Возпроизвести">Day</td>
-                                <td>День</td>
-                                <td></td>
-                                <td class="word" data-word="rain" title="Возпроизвести">Rain</td>
-                                <td>Дождь</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="eat" title="Возпроизвести">Eat</td>
-                                <td>Еда</td>
-                                <td></td>
-                                <td class="word" data-word="snow" title="Возпроизвести">Snow</td>
-                                <td>Снег</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="fan" title="Возпроизвести">Fan</td>
-                                <td>Поклонник</td>
-                                <td></td>
-                                <td class="word" data-word="text" title="Возпроизвести">Text</td>
-                                <td>Текст</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="gas" title="Возпроизвести">Gas</td>
-                                <td>Газ</td>
-                                <td></td>
-                                <td class="word" data-word="use" title="Возпроизвести">Use</td>
-                                <td>Использовать</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="hay" title="Возпроизвести">Hay</td>
-                                <td>Сено</td>
-                                <td></td>
-                                <td class="word" data-word="very" title="Возпроизвести">Very</td>
-                                <td>Очень</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="ice" title="Возпроизвести">Ice</td>
-                                <td>Лед</td>
-                                <td></td>
-                                <td class="word" data-word="jew" title="Возпроизвести">Jew</td>
-                                <td>Иудей</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="key" title="Возпроизвести">Key</td>
-                                <td>Ключ</td>
-                                <td></td>
-                                <td class="word" data-word="wait" title="Возпроизвести">wait</td>
-                                <td>Подождите</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="lip" title="Возпроизвести">Lip</td>
-                                <td>Губа</td>
-                                <td></td>
-                                <td class="word" data-word="x-ray" title="Возпроизвести">X-ray</td>
-                                <td>Рентген</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="my" title="Возпроизвести">My</td>
-                                <td>Мой</td>
-                                <td></td>
-                                <td class="word" data-word="yes" title="Возпроизвести">Yes</td>
-                                <td>Да</td>
-                            </tr>
-                            <tr>
-                                <td class="word" data-word="no" title="Возпроизвести">No</td>
-                                <td>Нет</td>
-                                <td></td>
-                                <td class="word" data-word="zoo" title="Возпроизвести">Zoo</td>
-                                <td>Зоопарк</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </div>
                 </div>
             </div>
-    </body>
+            <div>
+
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td>Word</td>
+                            <td>Translate</td>
+                            <td></td>
+                            <td>Word</td>
+                            <td>Translate</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="act" title="Возпроизвести">Act</td>
+                            <td>Акт</td>
+                            <td></td>
+                            <td class="word" data-word="old" title="Возпроизвести">Old</td>
+                            <td>Старый</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="bay" title="Возпроизвести">Bay</td>
+                            <td>Залив</td>
+                            <td></td>
+                            <td class="word" data-word="pen" title="Возпроизвести">Pen</td>
+                            <td>Ручка</td>
+                        </tr>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="city" title="Возпроизвести">City</td>
+                            <td>Город</td>
+                            <td></td>
+                            <td class="word" data-word="queen" title="Возпроизвести">Queen</td>
+                            <td>Королева</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="day" title="Возпроизвести">Day</td>
+                            <td>День</td>
+                            <td></td>
+                            <td class="word" data-word="rain" title="Возпроизвести">Rain</td>
+                            <td>Дождь</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="eat" title="Возпроизвести">Eat</td>
+                            <td>Еда</td>
+                            <td></td>
+                            <td class="word" data-word="snow" title="Возпроизвести">Snow</td>
+                            <td>Снег</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="fan" title="Возпроизвести">Fan</td>
+                            <td>Поклонник</td>
+                            <td></td>
+                            <td class="word" data-word="text" title="Возпроизвести">Text</td>
+                            <td>Текст</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="gas" title="Возпроизвести">Gas</td>
+                            <td>Газ</td>
+                            <td></td>
+                            <td class="word" data-word="use" title="Возпроизвести">Use</td>
+                            <td>Использовать</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="hay" title="Возпроизвести">Hay</td>
+                            <td>Сено</td>
+                            <td></td>
+                            <td class="word" data-word="very" title="Возпроизвести">Very</td>
+                            <td>Очень</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="ice" title="Возпроизвести">Ice</td>
+                            <td>Лед</td>
+                            <td></td>
+                            <td class="word" data-word="jew" title="Возпроизвести">Jew</td>
+                            <td>Иудей</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="key" title="Возпроизвести">Key</td>
+                            <td>Ключ</td>
+                            <td></td>
+                            <td class="word" data-word="wait" title="Возпроизвести">wait</td>
+                            <td>Подождите</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="lip" title="Возпроизвести">Lip</td>
+                            <td>Губа</td>
+                            <td></td>
+                            <td class="word" data-word="x-ray" title="Возпроизвести">X-ray</td>
+                            <td>Рентген</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="my" title="Возпроизвести">My</td>
+                            <td>Мой</td>
+                            <td></td>
+                            <td class="word" data-word="yes" title="Возпроизвести">Yes</td>
+                            <td>Да</td>
+                        </tr>
+                        <tr>
+                            <td class="word" data-word="no" title="Возпроизвести">No</td>
+                            <td>Нет</td>
+                            <td></td>
+                            <td class="word" data-word="zoo" title="Возпроизвести">Zoo</td>
+                            <td>Зоопарк</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
